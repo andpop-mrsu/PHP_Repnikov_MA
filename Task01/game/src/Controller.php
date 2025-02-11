8ac4ccd12aeaf9c509314ded09838cd99ab2ab35
maksim_repnikov <maks.repnikov32@mail.ru>
2025-02-11 23:18:12 +0300

    Task01

---
+++ Task01/game/src/Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Neospaer\prime\Controller;
+
+use Neospaer\prime\View;
+
+function startGame()
+{
+
+    View\displayStartScreen();
+}
---
+++ Task01/game/src/View.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Neospaer\prime\View;
+
+function displayStartScreen()
+{
+
+    echo "Hello man!\n";
+}
